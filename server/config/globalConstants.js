ba7fc490fe881b07a0f8bd94cb3d9904045b5d54
Duong Tran Tu Minh <duongtrantuminh14@gmail.com>
2020-05-03 15:44:20 +0700

    add global constance

---
+++ server/config/globalConstants.js
@@ -0,0 +1,17 @@
+const globalConstant = {
+  role: {
+    USER: "user",
+    ROOT: "root"
+  },
+  gender: {
+    FEMALE: "female",
+    MALE: "male",
+    OTHER: "other"
+  },
+  activate: {
+    ACTIVATED: 1,
+    DEACTIVATED: 0,
+    PENDING: 2
+  }
+};
+export default globalConstant;
