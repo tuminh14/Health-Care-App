85d3c882651bb56b4590b7af2400ca1ff65d1368
Duong Tran Tu Minh <duongtrantuminh14@gmail.com>
2020-05-03 15:42:43 +0700

    init model

---
+++ server/models/stepByDay.model.js
@@ -0,0 +1,37 @@
+import mongooge from "mongoose"
+import Step from "./step.model"
+import globalConstant from "../config/globalConstants";
+const StepByDaySchema = mongooge.Schema({
+    dayId: {type: mongooge.Types.ObjectId, ref: "Step"},
+    h01: {type: Number, default: 0},
+    h02: {type: Number, default: 0},
+    h03: {type: Number, default: 0},
+    h04: {type: Number, default: 0},
+    h05: {type: Number, default: 0},
+    h06: {type: Number, default: 0},
+    h07: {type: Number, default: 0},
+    h08: {type: Number, default: 0},
+    h09: {type: Number, default: 0},
+    h10: {type: Number, default: 0},
+    h11: {type: Number, default: 0},
+    h12: {type: Number, default: 0},
+    h13: {type: Number, default: 0},
+    h14: {type: Number, default: 0},
+    h15: {type: Number, default: 0},
+    h16: {type: Number, default: 0},
+    h17: {type: Number, default: 0},
+    h18: {type: Number, default: 0},
+    h19: {type: Number, default: 0},
+    h20: {type: Number, default: 0},
+    h21: {type: Number, default: 0},
+    h22: {type: Number, default: 0},
+    h23: {type: Number, default: 0},
+    h24: {type: Number, default: 0},
+
+},{timestamp: true});
+StepSchema.set("toJSON",{
+    transform(doc, ret, options){
+        delete ret.__v;
+    }
+});
+export default mongooge.model("Step", StepByDaySchema);
---
+++ server/models/users.model.js
@@ -0,0 +1,22 @@
+import mongooge from "mongoose"
+import globalConstant from "../config/globalConstants";
+const UserSchema = mongooge.Schema({
+    email: { type: String, required: true, unique: true },
+    fullName: { type: String, required: true },
+    passWord: { type: String, required: true },
+    phoneNumber: { type: String, unique: true},
+    gender: {type: String, default: globalConstant.gender.OTHER},
+    birthDay: {type: Date},
+    role: {type: String,  default: globalConstant.role.USER},
+    active: {type: Number, default: globalConstant.activate.DEACTIVATED},
+    online:{type: Boolean, default: false},
+    weight: {type: Number, required: true},
+    height: {type: Number, required: true}
+},{timestamp: true});
+UserSchema.set("toJSON",{
+    transform(doc, ret, options){
+        delete ret.__v;
+        delete ret.password;
+    }
+});
+export default mongooge.model("User", UserSchema);
