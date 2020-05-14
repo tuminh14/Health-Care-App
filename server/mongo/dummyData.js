a8367ab4af6d32ad50b7f9577d024e17718286ab
Duong Tran Tu Minh <duongtrantuminh14@gmail.com>
2020-05-14 09:25:30 +0700

    change dummy data

--- server/mongo/dummyData.js
+++ server/mongo/dummyData.js
@@ -2,18 +2,20 @@ import User from '../models/users.model';
 import globalConstant from "../config/globalConstants";
 export async function dummyUser(){
     try{
-        await User.create({
-            email: "duongtrantuminh14@gmail.com",
-            fullName: "Dương Trần Tử Minh",
-            passWord: "kOp1kpas",
-            phoneNumber: "0943686018",
-            gender: globalConstant.gender.MALE,
-            birthDay: Date.parse('14/06/1999'),
-            role: globalConstant.role.ROOT,
-            weight: 10,
-            height: 36
+        let existedUser = await User.findOne({email:"duongtrantuminh14@gmail.com"})
+        if(!existedUser){
+            await User.create({
+                email: "duongtrantuminh14@gmail.com",
+                fullName: "Dương Trần Tử Minh",
+                passWord: "kOp1kpas",
+                phoneNumber: "0943686018",
+                gender: globalConstant.gender.MALE,
+                birthDay: Date.now(),
+                role: globalConstant.role.ROOT,
+                weight: 10,
+                height: 36
         })
-        console.log("dummy data success")
+        }
     }catch(err){
         console.error(err)
     }
