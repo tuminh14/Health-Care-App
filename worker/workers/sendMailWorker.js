562aa3ce5678179d4c804cba62e5f00e20169d57
Duong Tran Tu Minh <duongtrantuminh14@gmail.com>
2020-06-17 16:13:54 +0700

    remove unnessecessary code

--- worker/workers/sendMailWorker.js
+++ worker/workers/sendMailWorker.js
@@ -6,26 +6,13 @@ import AMPQ from "../../ampq/ampq";
 AMPQ.consumeData(globalConstants.jobName.SEND_EMAIL, async (msg,channel)=>{
     try{
         const data = JSON.parse(msg.content.toString());
-        const existUser = await User.findOne({email: data.email}).lean();
-        if(existUser){
-            if(existUser.activeMail === globalConstants.activate.DEACTIVATED) {
-                const user = {
-                    fullName: existUser.fullName,
-                    email: existUser.email,
-                    token: data.token,
-                    cuid: existUser.cuid
-                }
-                await sendMail(user);
-                return true;
-            }
-            const err = new Error('User already activated');
-            console.log(`${globalConstants.jobName.SEND_EMAIL} fail`);
-            console.error(err);
-            return true;
+        const user = {
+            fullName: data.fullName,
+            email: data.email,
+            token: data.token,
+            cuid: data.cuid
         }
-        const err = new Error('Email Wrong');
-        console.log(`${globalConstants.jobName.SEND_EMAIL} fail`);
-        console.error(err);
+        await sendMail(user);
         return true;
     }catch(err){
         console.log(`${globalConstants.jobName.SEND_EMAIL} fail`);
